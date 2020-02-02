fcd17bf9f9160884eab214a255521f379745b95a
Jayson Grace <jayson.e.grace@gmail.com>
2020-02-01 16:06:41 -0800

    Add extended lab

---
+++ docker/extended/files/knife.rb
@@ -0,0 +1,7 @@
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "/root/.chef/admin.pem"
+validation_client_name   "extended_org-validator"
+chef_server_url          "https://chef-server/organizations/extended_org"
+cookbook_path            ["/root/.chef/cookbooks"]
