935f0c79e23db17deccab2b7c4423f88260670b1
Jayson Grace <jayson.e.grace@gmail.com>
2022-09-18 23:24:09 -0600

    Add github actions, magefile, pre-commit hooks, dependabot

---
+++ .mage/magefile.go
@@ -0,0 +1,75 @@
+//go:build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	goutils "github.com/l50/goutils"
+
+	// mage utility functions
+	"github.com/magefile/mage/mg"
+)
+
+func init() {
+	os.Setenv("GO111MODULE", "on")
+}
+
+// InstallDeps Installs go dependencies
+func InstallDeps() error {
+	fmt.Println(color.YellowString("Installing dependencies."))
+
+	if err := goutils.Tidy(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install dependencies: %v", err))
+	}
+
+	if err := goutils.InstallGoPCDeps(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install pre-commit dependencies: %v", err))
+	}
+
+	if err := goutils.InstallVSCodeModules(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install vscode-go modules: %v", err))
+	}
+
+	return nil
+}
+
+// InstallPreCommitHooks Installs pre-commit hooks locally
+func InstallPreCommitHooks() error {
+	mg.Deps(InstallDeps)
+
+	fmt.Println(color.YellowString("Installing pre-commit hooks."))
+	if err := goutils.InstallPCHooks(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RunPreCommit runs all pre-commit hooks locally
+func RunPreCommit() error {
+	mg.Deps(InstallDeps)
+
+	fmt.Println(color.YellowString("Updating pre-commit hooks."))
+	if err := goutils.UpdatePCHooks(); err != nil {
+		return err
+	}
+
+	fmt.Println(color.YellowString(
+		"Clearing the pre-commit cache to ensure we have a fresh start."))
+	if err := goutils.ClearPCCache(); err != nil {
+		return err
+	}
+
+	fmt.Println(color.YellowString("Running all pre-commit hooks locally."))
+	if err := goutils.RunPCHooks(); err != nil {
+		return err
+	}
+
+	return nil
+}
