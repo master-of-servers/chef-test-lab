3767f4fde71a4070d2351b08958d7becc09e6114
Jayson Grace <jayson.e.grace@gmail.com>
2022-12-19 15:10:55 -0700

    Update mage and fix dependabot PR permissions

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -6,13 +6,35 @@ import (
 	"fmt"
 	"os"
 
+	// mage utility functions
 	"github.com/fatih/color"
 	goutils "github.com/l50/goutils"
-	// mage utility functions
 )
 
 func init() {
 	os.Setenv("GO111MODULE", "on")
+}
+
+// InstallDeps Installs go dependencies
+func InstallDeps() error {
+	fmt.Println(color.YellowString("Installing dependencies."))
+
+	if err := goutils.Tidy(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install dependencies: %v", err))
+	}
+
+	if err := goutils.InstallGoPCDeps(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install pre-commit dependencies: %v", err))
+	}
+
+	if err := goutils.InstallVSCodeModules(); err != nil {
+		return fmt.Errorf(color.RedString(
+			"failed to install vscode-go modules: %v", err))
+	}
+
+	return nil
 }
 
 // InstallPreCommitHooks Installs pre-commit hooks locally
